3869d8f24fbe82714dfcb4979803af4401600407
Lynus63 <aureli1lv@gmail.com>
2015-01-08 18:59:52 +0100

    Commit aurel

---
+++ BusinessManager.cs
@@ -0,0 +1,24 @@
+﻿using EntitiesLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StubDataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class BusinessManager
+    {
+        private DalManager _dm;
+        private string _date;
+
+        public BusinessManager()
+        {
+            _dm = new DalManager();
+            _date = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        public List<Auteur> 
+    }
+}
